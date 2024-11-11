5213dd37f0866bfd3a987c4a2627d5d2b9858152
KonneyJ <konnova.j@yandex.ru>
2024-11-11 16:40:33 +0300

    Внесены изменения

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -21,7 +21,5 @@ public interface FilmStorage {
 
     Collection<Film> getCommonFilms(Long userId, Long friendId);
 
-    Collection<Film> getCommonFilms(Long userId, Long friendId);
-
     List<Film> getFilmsByDirector(Long id, String sortBy);
 }
