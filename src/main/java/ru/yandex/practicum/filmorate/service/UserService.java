010c30cb3042fb6239e6701f01ec16b99659ca09
hohlovvitali <b1aderaven@yandex.by>
2024-11-13 23:48:35 +0300

    ref checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.EventOperation;
 import ru.yandex.practicum.filmorate.model.EventType;
